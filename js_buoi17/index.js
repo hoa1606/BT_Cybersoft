61179357b33ee2a6bff2b1fb89fe705ea669d505
hoa1606 <dohuuhoa1606@gmail.com>
2024-05-19 22:29:49 +0700

    js buoi 17

---
+++ js_buoi17/index.js
@@ -0,0 +1,22 @@
+var btn1 = document.getElementById("bt_bai1");
+
+
+
+
+
+
+
+function for_bai1(){
+    var s = 0;
+    var i = 1;
+     
+    for(i; i<150; i++){
+        s = s+i;
+        if(s >10000){x
+            console.log(-- i +"\n"+  s);
+            break;
+        }
+    }
+}
+
+btn1.onclick = for_bai1;
