0978151465155deb416fe2c601c0f6961945394a
hoa1606 <dohuuhoa1606@gmail.com>
2024-06-14 22:22:31 +0700

    js buoi18 bai 2

--- js_buoi18_bootstrap/index.js
+++ js_buoi18_bootstrap/index.js
@@ -40,4 +40,46 @@ b1_bt3.onclick = function(){
 
 };
 
+// bai2
 
+// bai3
+
+let b3_bt1 = document.getElementById("tab_b3_bt1");
+let b3_bt2 = document.getElementById("tab_b3_bt2");
+let b3_bt3 = document.getElementById("tab_b3_bt3");
+let b3_content1 = document.getElementById("content_b3_1");
+let b3_content2 = document.getElementById("content_b3_2");
+let b3_content3= document.getElementById("content_b3_3");
+
+b3_bt1.onclick = function(){
+    b3_bt1.className += " active";
+    b3_content1.className += " active";
+
+    b3_bt2.classList.remove("active");
+    b3_content2.classList.remove("active");
+
+    b3_bt3.classLis.remove("active");
+    b3_content3.classList.remove("active")
+}
+
+b3_bt2.onclick = function(){
+    b3_bt2.className += " active";
+    b3_content2.className += " active";
+
+    b3_bt1.classList.remove("active");
+    b3_content1.classList.remove("active");
+
+    b3_bt3.classLis.remove("active");
+    b3_content3.classList.remove("active")
+}
+
+b3_bt3.onclick = function(){
+    b3_bt3.className += " active";
+    b3_content3.className += " active";
+
+    b3_bt2.classList.remove("active");
+    b3_content2.classList.remove("active");
+
+    b3_bt1.classLis.remove("active");
+    b3_content1.classList.remove("active")
+}
